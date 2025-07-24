9b355745a3912bc60d076db0245a769ebca2bc63
Bruce Li <isanoob1897@outlook.com>
2025-07-24 23:28:32 +0000

    add project files

---
+++ app/src/main/java/dev/desktop/App.java
@@ -0,0 +1,11 @@
+package dev.desktop;
+import java.util.*;
+import java.io.File;
+public class App {
+    public static void main(String[] args) {
+        xE(args);
+    }
+    public static void xE(String[] args) {
+
+    }
+}
